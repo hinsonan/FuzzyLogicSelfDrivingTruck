6735b83372d70ea3ccfd7ed459dfab92f04e4c75
Jeff Wilson <jeff@imtc.gatech.edu>
2021-03-22 19:23:05 -0400

    first check-in

---
+++ Assets/Scripts/GameAIStudentWork/FuzzyVehicle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GameAI;
+
+// All the Fuzz
+using Tochas.FuzzyLogic;
+using Tochas.FuzzyLogic.MembershipFunctions;
+using Tochas.FuzzyLogic.Evaluators;
+using Tochas.FuzzyLogic.Mergers;
+using Tochas.FuzzyLogic.Defuzzers;
+using Tochas.FuzzyLogic.Expressions;
+
+namespace GameAICourse
+{
+
+    public class FuzzyVehicle : AIVehicle
+    {
+
+        // TODO create some Fuzzy Set enumeration types, and member variables for:
+        // Fuzzy Sets (input and output), one or more Fuzzy Value Sets, and Fuzzy
+        // Rule Sets for each output.
+        // Also, create some methods to instantiate each of the member variables
+
+   
+        protected override void Awake()
+        {
+            base.Awake();
+
+            StudentName = "George P. Burdell";
+
+            // Only the AI can control. No humans allowed!
+            IsPlayer = false;
+
+            // TODO: You can initialize a bunch of Fuzzy stuff here
+
+
+        }
+
+
+        override protected void Update()
+        {
+
+            // TODO Do all your Fuzzy stuff here and pass the defuzzified values to 
+            // the car like so:
+            // Throttle = someValue; //[-1f, 1f] -1 is full brake, 0 is neutral, 1 is full throttle
+            // Steering = someValue; // [-1f, 1f] -1 if full left, 0 is neutral, 1 is full right
+
+
+
+            // recommend you keep the base call at the end, after all your FuzzyVehicle code so that
+            // control inputs can be processed properly (Throttle, Steering)
+            base.Update();
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameAIStudentWork/RacingTests/RacingTest.cs
@@ -0,0 +1,69 @@
+﻿
+using System.IO;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+using GameAI;
+
+namespace Tests
+{
+
+    public class RacingTest
+    {
+
+        const int timeScale = 5; // how fast to run the game. Running fast doesn't necessarily
+                                 // give accurate results.
+
+        const int PlayMatchTimeOutMS = int.MaxValue; // don't mess with this; add it to new tests
+                                                     // as [Timeout(PlayMatchTimeOutMS)] (see below for
+                                                     // example) It stops early default timeout
+
+        public RacingTest()
+        {
+           
+        }
+
+
+        [UnityTest]
+        [Timeout(PlayMatchTimeOutMS)]
+        public IEnumerator TestFuzzyRace()
+        {
+            return _TestFuzzyRace();
+        }
+
+
+        [Timeout(PlayMatchTimeOutMS)]
+        public IEnumerator _TestFuzzyRace()
+        {
+            Time.timeScale = timeScale;
+    
+            var sceneName = "RaceTrackFZ";
+
+            SceneManager.LoadScene(sceneName);
+
+            var waitForScene = new WaitForSceneLoaded(sceneName);
+            yield return waitForScene;
+
+            Assert.IsFalse(waitForScene.TimedOut, "Scene " + sceneName + " was never loaded");
+
+            yield return new WaitForSeconds(5f * 60f);
+
+            var gm = GameManager.Instance;
+
+            Assert.That(gm.KpHLTA, Is.GreaterThanOrEqualTo(40f));
+
+            Assert.That(gm.Wipeouts, Is.LessThanOrEqualTo(1));
+           
+
+        }
+
+    }
+
+}
+
